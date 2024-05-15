308d22ee01f8dfd3d545f1f9b9119c96f6f7b723
Parker092 <53531818+Parker092@users.noreply.github.com>
2024-05-15 17:39:08 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter a number between -9999 and 9999 (type 'EXIT' to quit):");
+            string input = Console.ReadLine();
+
+            if (input.ToUpper() == "EXIT")
+                break;
+
+            if (int.TryParse(input, out int number))
+            {
+                if (number >= -9999 && number <= 9999)
+                {
+                    Console.WriteLine(NumberToWords(number));
+                }
+                else
+                {
+                    Console.WriteLine("Number is out of the allowed range.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+    }
+
+    public static string NumberToWords(int number)
+    {
+        if (number == 0)
+            return "Zero";
+
+        if (number < 0)
+            return "Minus " + NumberToWords(Math.Abs(number));
+
+        string words = "";
+
+        if ((number / 1000) > 0)
+        {
+            words += NumberToWords(number / 1000) + " Thousand ";
+            number %= 1000;
+        }
+
+        if ((number / 100) > 0)
+        {
+            words += NumberToWords(number / 100) + " Hundred ";
+            number %= 100;
+        }
+
+        if (number > 0)
+        {
+            if (words != "")
+                words += "and ";
+
+            var unitsMap = new[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+            var tensMap = new[] { "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+            if (number < 20)
+                words += unitsMap[number];
+            else
+            {
+                words += tensMap[number / 10];
+                if ((number % 10) > 0)
+                    words += "-" + unitsMap[number % 10];
+            }
+        }
+
+        return words.Trim();
+    }
+}
